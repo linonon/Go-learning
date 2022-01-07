dc3df7c4bcd6a88fc7237f3bc2c279a93e3dee13
linonon <oliver@labsgl.com>
2022-01-07 13:48:53 +0800

    selectv1

---
+++ tdd/test/04-Select/select_test.go
@@ -0,0 +1,35 @@
+package select_test
+
+import (
+	"net/http"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	slowURL := "http://www.fackebook.com"
+	fastURL := "http://www.quii.co.uk"
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got '%s', want '%s'", got, want)
+	}
+}
+
+func Racer(a, b string) (winner string) {
+	startA := time.Now()
+	http.Get(a)
+	aDuration := time.Since(startA)
+
+	startB := time.Now()
+	http.Get(b)
+	bDuration := time.Since(startB)
+
+	if aDuration < bDuration {
+		return a
+	}
+
+	return b
+}
