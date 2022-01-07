a045e1fb91aecdff535a5ef0dfad2c0824bf430b
linonon <oliver@labsgl.com>
2022-01-07 13:52:21 +0800

    selectv2

--- tdd/test/04-Select/select_test.go
+++ tdd/test/04-Select/select_test.go
@@ -1,12 +1,22 @@
 package select_test
 
 import (
+	"fmt"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 	"time"
 )
 
 func TestRacer(t *testing.T) {
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
 	slowURL := "http://www.fackebook.com"
 	fastURL := "http://www.quii.co.uk"
 
@@ -16,6 +26,9 @@ func TestRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("got '%s', want '%s'", got, want)
 	}
+
+	slowServer.Close()
+	fastServer.Close()
 }
 
 func Racer(a, b string) (winner string) {
@@ -26,6 +39,8 @@ func Racer(a, b string) (winner string) {
 	startB := time.Now()
 	http.Get(b)
 	bDuration := time.Since(startB)
+
+	fmt.Println(a, aDuration, ";", b, bDuration)
 
 	if aDuration < bDuration {
 		return a
