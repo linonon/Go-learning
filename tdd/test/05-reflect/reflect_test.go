8f0495aa6d686ac2f88ea066738102682052c306
linonon <oliver@labsgl.com>
2022-01-10 10:24:58 +0800

    Learn

---
+++ tdd/test/05-reflect/reflect_test.go
@@ -0,0 +1,123 @@
+package reflect_test
+
+import (
+	"reflect"
+	"testing"
+)
+
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
+func TestWalk(t *testing.T) {
+	cases := []struct {
+		Name          string      // Test name
+		Input         interface{} // Test input
+		ExpectedCalls []string    // Test expected calls
+	}{
+		{
+			"Struct with one string field",
+			struct{ Name string }{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"Struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Struct with non string field",
+			struct {
+				Name string
+				Age  int
+			}{"Chris", 33},
+			[]string{"Chris"},
+		},
+		{
+			"Nested fields",
+			Person{"Chris", Profile{33, "London"}},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Pointers to things",
+			&Person{"Chris", Profile{33, "London"}},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Slices",
+			[]Profile{{33, "London"}, {34, "reykjavik"}},
+			[]string{"London", "reykjavik"},
+		},
+		{
+			"Arrays",
+			[2]Profile{{33, "London"}, {34, "reykjavik"}},
+			[]string{"London", "reykjavik"},
+		},
+		{
+			"Maps",
+			map[string]string{
+				"Foo": "Bar",
+				"Bar": "Boz",
+			},
+			[]string{"Bar", "Boz"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
+	}
+}
+
+func walk(x interface{}, fn func(input string)) {
+	val := getValue(x)
+
+	numberOfValues := 0
+	var getField func(int) reflect.Value
+
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		numberOfValues = val.NumField()
+		getField = val.Field
+	case reflect.Slice, reflect.Array:
+		numberOfValues = val.Len()
+		getField = val.Index
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walk(val.MapIndex(key).Interface(), fn)
+		}
+	}
+
+	for i := 0; i < numberOfValues; i++ {
+		walk(getField(i).Interface(), fn)
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	return val
+}
---
+++ tdd/test/06-HTTP-Server/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type StubPlayerStore struct {
+	scores   map[string]int
+	winCalls []string
+}
+
+func (s *StubPlayerStore) GetPlayerScore(name string) int {
+	return s.scores[name]
+}
+
+func (s *StubPlayerStore) RecordWin(name string) {
+	s.winCalls = append(s.winCalls, name)
+}
+
+type InMemoryPlayerStore struct {
+	store map[string]int
+}
+
+func NewInMemoryPlayerStore() *InMemoryPlayerStore {
+	return &InMemoryPlayerStore{map[string]int{}}
+}
+
+func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.store[name]++
+}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return i.store[name]
+}
+
+func main() {
+	server := &PlayerServer{&InMemoryPlayerStore{}}
+
+	if err := http.ListenAndServe(":5000", server); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
+}
