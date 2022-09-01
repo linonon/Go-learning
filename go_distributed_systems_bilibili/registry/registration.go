3b498de08b92b444dac6a3da92f2c87e03f57197
linonon-WSL2 <linonon@WSL2.com>
2022-09-01 13:11:26 +0800

    3.4

--- go_distributed_systems_bilibili/registry/registration.go
+++ go_distributed_systems_bilibili/registry/registration.go
@@ -1,11 +1,24 @@
 package registry
 
 type Registration struct {
-	ServiceName ServiceName
-	ServiceURL  string
+	ServiceName      ServiceName
+	ServiceURL       string
+	RequiredServices []ServiceName
+	ServiceUpdateURL string
 }
 type ServiceName string
 
 const (
-	LogService = ServiceName("LogService")
+	LogService     = ServiceName("LogService")
+	GradingService = ServiceName("GradingService")
 )
+
+type patchEnrty struct {
+	Name ServiceName
+	URL  string
+}
+
+type patch struct {
+	Added   []patchEnrty
+	Removed []patchEnrty
+}
